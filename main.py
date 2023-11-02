4870d5d8c9dc5b55155b0b416decbb1917cf5c04
Sofia Quintero <sofiaquinterovelez2017@gmail.com>
2023-11-02 08:05:47 -0500

    crea varias listas y muestra su tamaño, crear el .gitignore

---
+++ main.py
@@ -0,0 +1,30 @@
+
+
+
+edad = 12
+altura = 1.79
+nombre = "Sofia"
+estado = True
+
+
+
+
+lista1 = [10, 5, 3, 9]
+
+lista2 = [1.78, 2.66, 1.55, 89.4,6.9]
+
+lista3 = ["Lunes", "Martes", "Miercoles"]
+
+lista4 = ["Sofia", 45, 179]
+
+if __name__ == '__main__':
+
+    print(len(lista1))
+    print(len(lista2))
+    print(len(lista3))
+    print(len(lista4))
+
+    print()
+    print(lista1)
+    lista1[0] = 1
+    print()
